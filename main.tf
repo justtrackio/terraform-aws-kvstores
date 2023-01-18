4e7ef4a34a08cf1d5f29136babb8ef98f2265295
Sebastian Struß <31846129+applike-ss@users.noreply.github.com>
2023-01-18 14:34:11 +0100

    feat: added functionality (#2)
    ## Description
    <!--- Describe your changes in detail -->
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## Breaking Changes
    <!-- Does this break backwards compatibility with the current major
    version? -->
    <!-- If so, please provide an explanation why it is necessary. -->
    
    ## How Has This Been Tested?
    - [x] I have updated at least one of the `examples/*` to demonstrate and
    validate my change(s)
    - [x] I have tested and validated these changes using one or more of the
    provided `examples/*` projects
    <!--- Users should start with an existing example as its written, deploy
    it, then check their changes against it -->
    <!--- This will highlight breaking/disruptive changes. Once you have
    checked, deploy your changes to verify -->
    <!--- Please describe how you tested your changes -->
    - [x] I have executed `pre-commit run -a` on my pull request
    <!--- Please see
    https://github.com/antonbabenko/pre-commit-terraform#how-to-install for
    how to install -->

---
+++ main.tf
@@ -0,0 +1,63 @@
+module "kvstore_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0" # requires Terraform >= 0.13.0
+
+  for_each = var.kvstores
+
+  enabled             = module.this.enabled
+  namespace           = module.this.namespace
+  tenant              = module.this.tenant
+  environment         = module.this.environment
+  stage               = module.this.stage
+  name                = module.this.name
+  delimiter           = module.this.delimiter
+  tags                = module.this.tags
+  additional_tag_map  = module.this.additional_tag_map
+  label_order         = var.label_order
+  regex_replace_chars = module.this.regex_replace_chars
+  id_length_limit     = module.this.id_length_limit
+  label_key_case      = var.label_key_case
+  label_value_case    = var.label_value_case
+  descriptor_formats  = var.descriptor_formats
+  labels_as_tags      = var.labels_as_tags
+  attributes          = var.attributes_as_suffix ? concat([each.key], module.this.attributes) : concat(module.this.attributes, [each.key])
+}
+
+module "kvstore" {
+  source  = "justtrackio/kvstore/aws"
+  version = "1.1.3"
+
+  for_each = var.kvstores
+
+  context          = module.kvstore_label[each.key].context
+  label_value_case = var.label_value_case
+
+  name = var.name
+
+  ddb_autoscale_read_schedule  = each.value.ddb.autoscaler.enabled ? each.value.ddb.autoscaler.read_schedule : []
+  ddb_autoscale_write_schedule = each.value.ddb.autoscaler.enabled ? each.value.ddb.autoscaler.write_schedule : []
+  ddb_billing_mode             = each.value.ddb.billing_mode
+  ddb_label_order              = var.label_orders.ddb
+
+  iam_label_order = var.label_orders.iam
+
+  redis_cpu_size                           = each.value.redis.cpu_size
+  redis_deployment_maximum_percent         = each.value.redis.deployment.maximum_percent
+  redis_deployment_minimum_healthy_percent = each.value.redis.deployment.minimum_healthy_percent
+  redis_desired_count                      = each.value.redis.deployment.desired_count
+  redis_ecs_cluster_arn                    = var.redis_ecs_cluster_arn
+  redis_image_repository                   = each.value.redis.deployment.image.repository
+  redis_image_tag                          = each.value.redis.deployment.image.tag
+  redis_label_order                        = var.label_orders.redis
+  redis_launch_type                        = var.redis_launch_type
+  redis_memory_size                        = each.value.redis.memory_size
+  redis_network_mode                       = var.redis_network_mode
+  redis_propagate_tags                     = var.redis_propagate_tags
+  redis_service_discovery_dns_namespace_id = var.redis_service_discovery_dns_namespace_id
+  redis_service_memory_size                = each.value.redis.service_memory_size
+  redis_service_placement_constraints      = each.value.redis.service_placement_constraints
+  redis_vpc_id                             = var.redis_vpc_id
+  redis_enabled                            = each.value.redis.enabled
+
+  tags = module.this.tags
+}
